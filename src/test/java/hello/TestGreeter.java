b324306e4ec1252a6a8c16bdea3307d883050e74
dal79 <dal79@users.noreply.github.com>
2019-04-11 17:55:48 -0500

    Update TestGreeter.java
    added newtestDLGreeterPass

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -1,65 +1,43 @@
 package hello;
 
-
-
 import static org.junit.Assert.*;
-
-
-
 import org.junit.Before;
-
 import org.junit.Test;
-
-
-
-
 
 public class TestGreeter {
 
-
-
    private Greeter g;
 
-	
-
    @Before
-
    public void setUp() throws Exception 
 
    {
-
       g = new Greeter();
-
    }
 
-
-
    @Test
-
    public void testGreeterEmpty() 
-
    {
-
       assertEquals(g.getName(),"");
-
       assertEquals(g.sayHello(),"Hello!");
-
    }
-
-	
 
    @Test
-
    public void testGreeter() 
-
    {
-
       g.setName("World");
-
       assertEquals(g.getName(),"World");
-
       assertEquals(g.sayHello(),"Hello World!");
-
    }
 
+   @Test
+   public void newtestDLGreeterPass() 
+   {
+      g.setName("World");
+      assertEquals(g.sayHello(),"Hello World!");
+      g.setName("");
+      assertEquals(g.sayHello(),"Hello!");
+      
+
+   }
 }
