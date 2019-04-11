1ba84c271be6b95fe423d7dddcdd93297b114640
Carter Kindred <C_k133@txstate.edu>
2019-04-11 18:51:10 -0500

    this is the revised fail test

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -75,7 +75,7 @@ public class TestGreeter {
    public void newtestCKGreeterFail()
    {
       g.setName("Hey its Carter"); 
-      assertEquals(g.setName(), "Hey its Carter");
+      assertEquals(g.sayHello(), "hey its Carter");
    }
 
 }
