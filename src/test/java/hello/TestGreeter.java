69b5eeb62b8968bd82078628cd3ec9b3029e61f6
Carter Kindred <C_k133@txstate.edu>
2019-04-26 20:58:53 -0500

    new testGreeter.java
    new testGreeter file

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -1,65 +1,34 @@
 package hello;
 
-
-
-import static org.junit.Assert.*;
-
-
-
-import org.junit.Before;
-
-import org.junit.Test;
-
-
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 
 public class TestGreeter {
 
-
-
-   private Greeter g;
-
-	
-
-   @Before
-
-   public void setUp() throws Exception 
-
-   {
-
-      g = new Greeter();
-
-   }
-
-
+   private Greeter g = new Greeter();
 
    @Test
-
+   @DisplayName("Test for Empty Name")
    public void testGreeterEmpty() 
 
    {
-
       assertEquals(g.getName(),"");
-
       assertEquals(g.sayHello(),"Hello!");
-
    }
 
-	
+
 
    @Test
-
+   @DisplayName("Test for Name='World'")
    public void testGreeter() 
-
    {
 
       g.setName("World");
-
       assertEquals(g.getName(),"World");
-
       assertEquals(g.sayHello(),"Hello World!");
-
    }
 
 }
